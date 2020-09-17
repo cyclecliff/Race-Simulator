c50000525fe532d23fb8df5eca3ed7512e23c759
Lubbert Romkes <57623149+cyclecliff@users.noreply.github.com>
2020-09-17 10:50:18 +0200

    Initial commit

---
+++ Model/IEquipment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Model
+{
+    interface IEquipment 
+    {
+        int Quality     { get; set; }
+        int Performance { get; set; }
+        int Speed       { get; set; }
+        bool IsBroken   { get; set; }
+
+    }
+}
---
+++ Model/IParticipant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    interface IParticipant
+    {
+        string Name             { get; set; }
+        int Points              { get; set; }
+        IEquipment Equipment    { get; set; }
+        TeamColors TeamColor    { get; set; }
+
+
+
+    }
+}
