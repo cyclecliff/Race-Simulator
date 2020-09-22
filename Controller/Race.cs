92946d542845bca44542bb123f214a1031e35f09
Lubbert Romkes <57623149+cyclecliff@users.noreply.github.com>
2020-09-22 16:38:36 +0200

    lolwut

---
+++ Controller/Race.cs
@@ -0,0 +1,23 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Controller
+{
+    public class Race
+    {
+        public  Track track;
+        public  List<IParticipant> Participants;
+        public  DateTime StartTime;
+        private Random _random;
+        private Dictionary<Section, SectionData> _positions;
+
+        public SectionData GetSectionData(Section section)
+        { //als op section plek geen value
+            SectionData _sectiondata = _positions[section];
+
+            return _sectiondata;
+        }
+    }
+}
